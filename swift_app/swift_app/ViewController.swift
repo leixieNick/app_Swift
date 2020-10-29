99fc036d117fd3c16e459933ba11c823e185ffbb
xielei <lei_xie@asdc.com.cn>
2020-10-29 17:22:05 +0800

    工程初始化

---
+++ swift_app/swift_app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  swift_app
+//
+//  Created by asdc on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
